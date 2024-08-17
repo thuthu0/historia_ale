e9c6bbab43fc16a2d2825c279b018e9c730b91cd
thuthu0 <100043083+thuthu0@users.noreply.github.com>
2024-08-17 20:45:26 -0300

    Add files via upload

---
+++ tecnicas-computacionais-refletindo-sobre-ia-main/js/perguntas.js
@@ -0,0 +1,155 @@
+export const perguntas = [
+    {
+        enunciado: "Amanheceu, o despertador tocou",
+        alternativas: [ 
+            {
+                texto: "levantei",
+                afirmacao: [
+                    "Fui me arrumar para ir para o colégio",
+                    "Tomei café, escovei os meus dentes, peguei minha mochila e saí."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "dormir",
+                afirmacao: [
+                    "Programei o despertador para cinco minutos depois."
+                
+                ],
+                proxima: 2,
+            },
+            ]
+    },
+        
+    {
+        enunciado: "Fui até o terminal e esperei o ônibus para ir para o colégio, mas o ônibus atrasou",
+        alternativas: [
+            {
+                texto: "Fiquei esperando",
+                afirmacao: [
+                    "Quando o ônibus chegou eu entrei."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Fui embora.",
+                afirmacao: [
+                    "Comecei a caminhar, para algum lugar."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O despertador tocou novamente",
+        alternativas: [
+            {
+                texto: "Peguei e arremecei o despertador na parede",
+                afirmacao: [
+                    "Tive belos sonhos"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "decidi levantar",
+                afirmacao: [
+                    "sentei na cadeira e comecei a mexer no computador."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Cheguei no colégio e tive as primeiras aulas, e chegou o intervalo",
+        alternativas: [
+            {
+                texto: "Fui para a biblioteca.",
+                afirmacao: [
+                    "Li alguns livros."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Fui para o refeitório.",
+                afirmacao: [
+                    "Comi bastante."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Na caminhada, eu vi uma quadra e uma lan house.",
+        alternativas: [
+            {
+                texto: "Entrei na quadra.",
+                afirmacao: [
+                    "A quadra estava vazia."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Entrei na lan house.",
+                afirmacao: [
+                    "Ao entrar, a energia caiu."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Na quadra estava pensando se saia ou não.",
+        alternativas: [
+            {
+                texto: "Saí.",
+                afirmacao: [
+                    "Voltei a caminhar, estava quase  noite"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Fiquei",
+                afirmacao: [
+                    "Continuei exploranto a quadra, mas estava ficando de noite."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Na la house estava pensando se jogava xadrez ou cubo mágico",
+        alternativas: [
+            {
+                texto: "Xadrez.",
+                afirmacao: [
+                    "Joguei uma partida de xadrez, depois dessa partida, anoiteceu."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Mexi no cubo mágico.",
+                afirmacao: [
+                    "Fiquei tentando resolver o cubo mágico, mas estava anoitecendo e então parei."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao anoitecer, eu estava em casa, e refleti se o dia foi bom",
+        alternativas: [
+            {
+                texto: "Foi bom.",
+                afirmacao: [
+                    "Dormi feliz."
+                ],
+            },
+            {
+                texto: "Foi ruim.",
+                afirmacao: [
+                    "Dormi triste"
+                ],
+            },
+        ]
+    }
+];
